e1572bb9f5520a118388621d6d8d861b6c07ab97
Daniel Joo <askjoo@gmail.com>
2015-02-18 21:35:52 -0500

    Delete secret_token.rb

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-SimplematerialCom::Application.config.secret_token = '469e9d5624010b56f09cdedfdf84d17df8ab2b6e406fb6018251d29431ee0ed44391d324713fdcd0dea16b1c0cfe00bb047f7b33dc5627922dfaa0d148ad0fc8'
